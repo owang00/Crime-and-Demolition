ac41567047bd0817fdb9a19330f1c87f83b24171
Oliver Wang <ow361@nyu.edu>
2022-04-17 18:34:43 -0400

    Update some code. Should be mostly working for now

---
+++ Clean.java
@@ -0,0 +1,27 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class Clean {
+
+  public static void main(String[] args) throws Exception {
+    Job job = new Job();
+    job.setNumReduceTasks(1); // 1 Reduce task
+    job.setJarByClass(CountRecs.class);
+    job.setJobName("Clean");
+
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    
+    job.setMapperClass(CleanMapper.class);
+    job.setReducerClass(CleanReducer.class);
+
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
